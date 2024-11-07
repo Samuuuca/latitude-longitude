e51f5a104c25be90e96c27e9d7c485ac12d76aa7
Samuuuca <samueldelphino12@gmail.com>
2024-11-06 23:52:45 -0300

    commit inicial

---
+++ main.py
@@ -0,0 +1,27 @@
+from geopy.geocoders import Nominatim
+import pandas as pd
+
+def lerExecel():
+    excel = pd.read_excel(io='./enderecos.xlsx', index_col=0)
+    excel['LATITUDE'] = excel['LATITUDE'].astype(object)
+    excel['LONGITUDE'] = excel['LONGITUDE'].astype(object)
+    LINHA = 0
+    
+    for row in excel.itertuples(index=True, name=None):
+
+        LINHA += 1
+        endereco = f"{row[3]}, {row[5]} {row[6]},SP"
+        print(endereco)
+
+        latlong = getLatLon(endereco)
+        excel.at[LINHA,'LATITUDE'] = latlong[0]
+        excel.at[LINHA,'LONGITUDE'] = latlong[1]
+        
+    excel.to_excel("enderecos_CORRIGIDOS.xlsx")   
+
+def getLatLon(adress):
+    
+    geolocator = Nominatim(user_agent='samueldelphino12@gmail.com')
+    localizacao = geolocator.geocode(adress)
+    
+    return [localizacao.latitude, localizacao.longitude]
